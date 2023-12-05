d81188a2ee11ddb16a0089968c1f5654a1a0c2e9
Tony2803 <140980036+Tony2803@users.noreply.github.com>
2023-12-05 19:02:35 -0300

    innit

---
+++ calculadora.py
@@ -0,0 +1,41 @@
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    if b == 0:
+        return "Error: No se puede dividir por cero"
+    return a / b
+
+while True:
+    print("Opciones:")
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicación")
+    print("4. División")
+    print("5. Salir")
+
+    opcion = int(input("Seleccione una opción: "))
+
+    if opcion == 5:
+        print("¡Hasta luego!")
+        break
+
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+
+    if opcion == 1:
+        print("Resultado:", suma(num1, num2))
+    elif opcion == 2:
+        print("Resultado:", resta(num1, num2))
+    elif opcion == 3:
+        print("Resultado:", multiplicacion(num1, num2))
+    elif opcion == 4:
+        print("Resultado:", division(num1, num2))
+    else:
+        print("Opción inválida. Por favor, seleccione una opción válida.")
